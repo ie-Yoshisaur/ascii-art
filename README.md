62793ee166a7b23bcb48de7534d6a0c2494c7f42
e205723 <e205723@ie.u-ryukyu.ac.jp>
2023-10-19 19:41:59 +0900

    remote execution

---
+++ README.md
@@ -0,0 +1,3 @@
+`$ bash `create_remote_script.sh`でframe1.txt, frame2.txt, ...のファイルの中身のファイルを1フレーム0.05秒の頻度で表示してアニメーションが作れます。
+
+`$ bash remote_script.sh`でアニメーションを再生できます。
---
+++ create_remote_script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo '#!/bin/bash' > remote_script.sh
+echo '' >> remote_script.sh
+
+for frame_file in frame*.txt; do
+    frame_number="${frame_file//[^0-9]/}"
+    frame_variable="frame${frame_number}"
+
+    {
+        echo "read -r -d '' $frame_variable <<'EOF'"
+        cat "$frame_file"
+        echo 'EOF'
+        echo
+    } >> remote_script.sh
+done
+
+echo 'frames=(' >> remote_script.sh
+for frame_file in frame*.txt; do
+    frame_number="${frame_file//[^0-9]/}"
+    echo "\"\$frame${frame_number}\"" >> remote_script.sh
+done
+echo ')' >> remote_script.sh
+
+cat >> remote_script.sh <<'EOF'
+
+while true; do
+  for frame in "${frames[@]}"
+  do
+    clear
+    echo -e "$frame"
+    sleep 0.05
+  done
+done
+EOF
+
+chmod +x remote_script.sh
